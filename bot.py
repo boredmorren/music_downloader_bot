8f40730098a03528b2dae260c60a8956846ffa7c
boredmorren <chilleam.biz@gmail.com>
2024-07-17 10:24:56 +0300

    edit algorithm for downloading and sending tracks

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ def welcome(message):
 @bot.message_handler()
 def print_tracks(message, f=None):
 
+    global tracks_list 
     tracks_list = print_all_tracks(message.text)
 
     markup = telebot.types.InlineKeyboardMarkup()
@@ -20,17 +21,11 @@ def print_tracks(message, f=None):
         markup.add(telebot.types.InlineKeyboardButton(text=track_title, callback_data=track_title))
 
 
-    bot.send_message(message.chat.id, text='Выберите подходящий трек:', reply_markup=markup)
-
-    if f == 'inline':
-        return tracks_list
-
-        
+    bot.send_message(message.chat.id, text='Выберите подходящий трек:', reply_markup=markup)      
     
 
 @bot.callback_query_handler(func=lambda call: True)
 def send_track(call):
-    tracks_list = print_tracks('', 'inline')
     for key, value in tracks_list.items():
         if call.data == value:
             track = key;
@@ -40,6 +35,6 @@ def send_track(call):
     track_file = open(filename, 'rb')
     bot.answer_callback_query(call.id, "Download.")
 
-    bot.send_audio(call.chat.id, track_file)
+    bot.send_audio(call.message.chat.id, track_file)
 
 bot.infinity_polling()
